73c14cc61e294ae1ddf00bcf894b29ec2fcb26ad
Pavel-Suchkou <suchkovp@gmail.com>
2022-11-05 20:38:40 +0300

    added login token and middlewares

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import config from "../config.js";
+
+const roleMiddleware = (roles) => {
+    return function (req, res, next) {
+        if (req.method === 'OPTIONS') {
+            next()
+        }
+
+        try {
+            const token = req.headers.authorization.split(" ")[1]
+            if (!token) {
+                return res.status(403).json({message: "User is not authorized"})
+            }
+            const {roles: userRoles} = jwt.verify(token, config.secret)
+            let hasRole = false
+            userRoles.forEach(role => {
+                if (roles.includes(role)) {
+                    hasRole = true
+                }
+            })
+            if(!hasRole){
+                return res.status(403).json({message: "You have no permissions for it"})
+            }
+            next()
+        } catch (e) {
+            console.log(e)
+            return res.status(403).json({message: "User is not authorized"})
+        }
+    }
+}
+
+export default roleMiddleware
